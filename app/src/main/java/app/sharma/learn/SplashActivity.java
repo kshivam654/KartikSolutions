9f09eabddf62446e5aa1d00312bb7023fe5b71b4
Shivam Sharma <kshivam654@gmail.com>
2020-07-21 12:31:35 +0530

    activity_splash.xml the layout is done and the logo is placed

--- app/src/main/java/app/sharma/learn/SplashActivity.java
+++ app/src/main/java/app/sharma/learn/SplashActivity.java
@@ -1,5 +1,6 @@
 package app.sharma.learn;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,13 +9,27 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
